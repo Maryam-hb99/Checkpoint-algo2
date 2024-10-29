6f5e365f96ae555b6e0b8731b5e061954ccb1cd4
Maryam Hbab <maryamhbab2335@gmail.com>
2024-10-29 17:45:36 +0100

    Add new files

---
+++ main.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let current = arr[i];
+      let j = i - 1;
+  
+      while (j >= 0 && arr[j] > current) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      
+      arr[j + 1] = current;
+    }
+  
+    return arr;
+  }
+  
+  const array = [10, 2, 9, 1, 5, 6,7];
+  console.log("Sorted Array:", insertionSort(array));
+  
